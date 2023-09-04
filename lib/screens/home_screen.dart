a876213b4e7bb68927db255b4576c2307dca3bac
Yaseen <syedyaseen1539@gmail.com>
2023-09-04 13:09:21 +0530

    add firebase

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,11 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:shopping_list/data/categories.dart';
 import 'package:shopping_list/models/grocery_item.dart';
 import 'package:shopping_list/screens/new_item_screen.dart';
 import 'package:shopping_list/widgets/grocery_item.dart';
+import 'package:http/http.dart' as http;
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -11,12 +15,83 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  final List<GroceryItem> _groceryItems = [];
+  String? _error;
+  var _isLoading = true;
+  List<GroceryItem> _groceryItems = [];
+
+  void _deleteItem(GroceryItem groceryItem) async {
+    final index = _groceryItems.indexOf(groceryItem);
+    setState(() {
+      _groceryItems.remove(groceryItem);
+    });
+
+    final url = Uri.https("flutter-prep-c19e7-default-rtdb.firebaseio.com",
+        'shopping-list/${groceryItem.id}.json');
+    var response = await http.delete(url);
+
+    if (response.statusCode >= 400) {
+      setState(() {
+        _groceryItems.insert(index, groceryItem);
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _loadItems();
+  }
+
+  void _loadItems() async {
+    final url = Uri.https(
+        "flutter-prep-c19e7-default-rtdb.firebaseio.com", 'shopping-list.json');
+
+    try {
+      final response = await http.get(url);
+      if (response.statusCode >= 400) {
+        setState(() {
+          _error = "Failed to fetch data. Please try again later";
+        });
+      }
+
+      if (response.body == 'null') {
+        setState(() {
+          _isLoading = false;
+        });
+        return;
+      }
+      final Map<String, dynamic> listData = json.decode(response.body);
+      final List<GroceryItem> loadedItems = [];
+      for (final item in listData.entries) {
+        final _category = categories.entries.firstWhere(
+            (catItem) => catItem.value.category == item.value['category']);
+        loadedItems.add(
+          GroceryItem(
+            id: item.key,
+            name: item.value['name'],
+            quantity: item.value['quantity'],
+            category: _category.value,
+          ),
+        );
+      }
+      setState(() {
+        _groceryItems = loadedItems;
+        _isLoading = false;
+      });
+    } catch (error) {
+      setState(() {
+        _error = "Something went wrong. Please try again later";
+      });
+    }
+  }
+
   void _onAddItem() async {
     final newItem = await Navigator.of(context).push<GroceryItem>(
         MaterialPageRoute(builder: (ctx) => const NewItemScreen()));
+    // _loadItems();
     if (newItem == null) {
-      return null;
+      return;
     } else {
       setState(() {
         _groceryItems.add(newItem);
@@ -29,6 +104,18 @@ class _HomeScreenState extends State<HomeScreen> {
     Widget fallBackWidget = const Center(
       child: Text("No Groceries found. Try adding some groceries"),
     );
+
+    if (_isLoading) {
+      fallBackWidget = const Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+
+    if (_error != null) {
+      fallBackWidget = Center(
+        child: Text(_error!),
+      );
+    }
     return Scaffold(
       appBar: AppBar(
         title: const Text("Your groceries"),
@@ -50,9 +137,7 @@ class _HomeScreenState extends State<HomeScreen> {
                       color: Colors.red,
                     ),
                     onDismissed: (direction) {
-                      setState(() {
-                        _groceryItems.remove(_groceryItems[index]);
-                      });
+                      _deleteItem(_groceryItems[index]);
                     },
                     key: ValueKey(_groceryItems[index].id),
                     child: SingleGroceryItem(
--- lib/screens/new_item_screen.dart
+++ lib/screens/new_item_screen.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:shopping_list/data/categories.dart';
 import 'package:shopping_list/models/category.dart';
+
+import 'package:http/http.dart' as http;
 import 'package:shopping_list/models/grocery_item.dart';
 
 class NewItemScreen extends StatefulWidget {
@@ -15,18 +19,39 @@ class _NewItemState extends State<NewItemScreen> {
   var _enteredName = '';
   var _enteredQuantity = 1;
   var _selectedCategory = categories[Categories.vegetables]!;
-  void _saveItem() {
+  var _isSending = false;
+  void _saveItem() async {
     if (_formKey.currentState!.validate()) {
+      setState(() {
+        _isSending = true;
+      });
       _formKey.currentState!.save();
-      print(_enteredName);
-      print(_enteredQuantity);
-      print(_selectedCategory);
+      final url = Uri.https("flutter-prep-c19e7-default-rtdb.firebaseio.com",
+          'shopping-list.json');
+      final response = await http.post(
+        url,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: json.encode(
+          {
+            'name': _enteredName,
+            'quantity': _enteredQuantity,
+            'category': _selectedCategory.category,
+          },
+        ),
+      );
+
+      final Map<String, dynamic> resData = json.decode(response.body);
+
+      if (!context.mounted) {
+        return;
+      }
       Navigator.of(context).pop(GroceryItem(
-        id: DateTime.now().toString(),
-        name: _enteredName,
-        quantity: _enteredQuantity,
-        category: _selectedCategory,
-      ));
+          id: resData['name'],
+          name: _enteredName,
+          quantity: _enteredQuantity,
+          category: _selectedCategory));
     }
   }
 
@@ -44,7 +69,7 @@ class _NewItemState extends State<NewItemScreen> {
               children: [
                 TextFormField(
                   maxLength: 50,
-                  decoration: InputDecoration(
+                  decoration: const InputDecoration(
                     label: Text("Name"),
                   ),
                   validator: (value) {
@@ -67,7 +92,7 @@ class _NewItemState extends State<NewItemScreen> {
                     Expanded(
                       child: TextFormField(
                         keyboardType: TextInputType.number,
-                        decoration: InputDecoration(
+                        decoration: const InputDecoration(
                           label: Text("Quanity"),
                         ),
                         initialValue: _enteredQuantity.toString(),
@@ -130,12 +155,22 @@ class _NewItemState extends State<NewItemScreen> {
                   mainAxisAlignment: MainAxisAlignment.end,
                   children: [
                     TextButton(
-                        onPressed: () {
-                          _formKey.currentState!.reset();
-                        },
+                        onPressed: _isSending
+                            ? null
+                            : () {
+                                _formKey.currentState!.reset();
+                              },
                         child: const Text("Reset")),
                     ElevatedButton(
-                        onPressed: _saveItem, child: const Text("Add Item"))
+                      onPressed: _isSending ? null : _saveItem,
+                      child: _isSending
+                          ? const SizedBox(
+                              height: 16,
+                              width: 16,
+                              child: CircularProgressIndicator(),
+                            )
+                          : const Text("Add Item"),
+                    )
                   ],
                 )
               ],
